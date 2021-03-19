3390ff0406643fec65fddeb3a9fb17419d97346e
NRyden <nick.ryden@hotmail.com>
2021-03-20 10:06:36 +1100

    Updated js

--- engine.js
+++ engine.js
@@ -16,80 +16,115 @@ $.getJSON(maindir + "config.json", function( data ) {
 $("#design-wrapper").height = screen.height;
 $("#design-wrapper").width = screen.width;
 
-// Parse in the scene file - name is static.
-jQuery.get(LocnScene + 'title.scene', function (data) {
-    var lines = data.split("\n");
+function ProcessScene(Scene) {
+	// Parse in the scene file - name is static.
+	jQuery.get(LocnScene + Scene + ".scene", function (data) {
+		var lines = data.split("\n");
 
-    $.each(lines, function (key, val) {
-		if (val.charAt(0) == '[') {
-			// Clean the string of [, ] and split it up into meaningful values
-			CleanStr = val.replace('[', '');
-			CleanStr = CleanStr.replace(']', '');
-			Words = CleanStr.split(' ');
+		$.each(lines, function (key, val) {
+			if (val.charAt(0) == '[') {
+				// Clean the string of [, ] and split it up into meaningful values
+				CleanStr = val.replace('[', '');
+				CleanStr = CleanStr.replace(']', '');
+				Words = CleanStr.split(' ');
 
-			for (i = 1; i < Words.length; i++) {
-				item = Words[i].split('=');
-				
-				// This is where all the action and scheduling happens
-				switch (Words[0]) {
-					case 'title':
-						if (Words[0] == 'title' && item[0] == 'source') {
-							// Fade the title screen in and out
-							$('#wall').append('<div id="title">' + Words[i] + '</div>');
-							$('#title').load(LocnUI + item[1].replaceAll('"', ''));
-							$("#title").hide().fadeIn(1000);
-							$('#title').delay(5000);
-							$('#title').fadeOut(1000);
-						} else if (item[0] == 'transition') {
-							// Currently just a placeholder for more functionality down the line
-							$('#wall').append('<div>Im a banana</div><br />');
-						}
-						break;
-					case 'menu':
-						// Lods the menu 5s after the title screen has loaded
-						if (Words[0] == 'menu' && item[0] == 'source') {
-							$('#wall').append('<div id="menu">' + Words[1] + '</div>');
-							$("#menu").hide();
-							// Title FadeIn Time + Delay Time + FadeOut Time
-							$('#menu').delay(7000);
-							$("#menu").fadeIn(500);
-							$('#menu').load(LocnUI + item[i].replaceAll('"', ''));
-						}
-						break;
-					// Room for more functionality down the line
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
-					case 'placeholder1':
-						break;
+				for (i = 1; i < Words.length; i++) {
+					item = Words[i].split('=');
+					
+					// This is where all the action and scheduling happens
+					switch (Words[0]) {
+						case 'title':
+							if (item[0] == 'source') {
+								// Fade the title screen in and out
+								$('#wall').append('<div id="title">' + Words[i] + '</div>');
+								$('#title').load(LocnUI + item[1].replaceAll('"', ''));
+								$("#title").hide().fadeIn(1000);
+								$('#title').delay(5000);
+								$('#title').fadeOut(1000);
+							} else if (item[0] == 'transition') {
+								// Currently just a placeholder for more functionality down the line
+								$('#wall').append('<div>Im a banana</div><br />');
+							}
+							break;
+						case 'menu':
+							// Lods the menu 5s after the title screen has loaded
+							if (item[0] == 'source') {
+								$('#wall').append('<div id="menu">' + Words[1] + '</div>');
+								$("#menu").hide();
+								// Title FadeIn Time + Delay Time + FadeOut Time
+								$('#menu').delay(7000);
+								$("#menu").fadeIn(500);
+								$('#menu').load(LocnUI + item[i].replaceAll('"', ''));
+								
+								$('#menu').load(LocnUI + item[i].replaceAll('"', ''));
+							}
+							break;
+						// Room for more functionality down the line
+						case 'scene':
+							if (item[0] == 'source') {
+								$("#menu").hide();
+								$("#title").hide();
+								$('#wall').load(LocnUI + item[1].replaceAll('"', ''));
+							}
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+						case 'placeholder1':
+							break;
+					}
 				}
 			}
-		}
-    });
-});
+		});
+	});
+}
+
+
+
+
+
+
+function newGame() {
+	ProcessScene('arc1');
+}
+
+
+function contGame() {}
+
+
+function openPrefs() {}
+
+
+function openCredits() {}
+
+
+function doRageQuit() {}
+
+
+
+
+
 
 // if first char is { then...
 	//break words into arrays
@@ -101,4 +136,27 @@ jQuery.get(LocnScene + 'title.scene', function (data) {
 	// set up yes no options
 	// fork options etc
 // end if
-	
+
+
+
+ProcessScene('title');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
